5227a5a0cec04c34a776da0bf82e628f67d717f7
Huy-Ngo <duchuy29092000@gmail.com>
2020-01-31 09:55:58 +0700

    Define dependencies and declare setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='reddit-transtool',
+    version='0.1.0',
+    packages=['crawler'],
+    url='',
+    license='MIT',
+    author='Ngo Ngoc Duc Huy',
+    author_email='duchuy29092000@gmail.com',
+    description='Tool for translating or crawling reddit post'
+)
